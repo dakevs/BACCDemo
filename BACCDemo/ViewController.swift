f2a6a895a81aedf2e75483ce0dbdb41bd642882b
QA <qa@Yifengs-MacBook-Air.local>
2015-07-30 02:37:07 -0700

    Initial Commit

---
+++ BACCDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BACCDemo
+//
+//  Created by QA on 7/30/15.
+//  Copyright (c) 2015 ShepherdsSoftware. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
