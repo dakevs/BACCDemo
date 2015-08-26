958336b4cb65b9c0e7b7478b4980ca0f2621fc5c
dakevs <qa@Yifengs-MacBook-Air.local>
2015-08-26 04:02:46 -0700

    changes to the login page
    implemented hide/show confirm password field
    implemented password verification to ensure they match
    setup the logic to choose between the login/signup flow

--- BACCDemo/LoginViewController.swift
+++ BACCDemo/LoginViewController.swift
@@ -12,20 +12,61 @@ import Parse
 class LoginViewController: UIViewController {
     
 
+    var registerUser = false
+    
+    var activityIndicator: UIActivityIndicatorView = UIActivityIndicatorView()
+    
     @IBOutlet var txtUsername: UITextField!
     
     @IBOutlet var txtPassword: UITextField!
     
     @IBOutlet var txtConfirmPassword: UITextField!
     
+    @IBOutlet var Switch: UISwitch!
     
-    @IBAction func switchAction(value: Bool) {
-        if value{
+   
+    @IBAction func switchAction(sender: UISwitch) {
+        if Switch.on {
+            //user is in signup mode
             txtConfirmPassword.hidden = false
-        }else{
+        } else {
+            //user is in login mode
             txtConfirmPassword.hidden = true
-            
         }
+    }
+    
+    func displayAlert(title: String, message: String) {
+        
+        var alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction((UIAlertAction(title: "OK", style: .Default, handler: { (action) -> Void in
+            
+            self.dismissViewControllerAnimated(true, completion: nil)
+            
+        })))
+        
+        self.presentViewController(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func btnSubmit(sender: UIButton) {
+        
+        if Switch.on {
+            //user is in signup mode
+            if txtPassword.text != txtConfirmPassword.text {
+                //the password fields do not match
+                displayAlert("Mismatched Passwords", message:"Please enter matching passwords")
+           
+            } else {
+            //the password fields do match, and the user can register with this username/email and password
+            
+            }
+            
+        } else {
+            //user is in login mode and we can submit credentials
+        }
+    }
+    
+    @IBAction func btnForgotPassword(sender: UIButton) {
+        //after use taps this button the forgot password flow is begun
     }
     
     override func viewDidLoad() {
@@ -36,10 +77,10 @@ class LoginViewController: UIViewController {
     
     
     override func viewDidAppear(animated: Bool) {
-//        if PFUser.currentUser() != nil {
-            //that means a user is logged in, and then we perform the segue to the map view
-  //          self.performSegueWithIdentifier("gotoMap", sender: self)
-        
+
+        //if PFUser.currentUser() != nil {
+        //that means a user is logged in, and then we perform the segue to the map view
+        //self.performSegueWithIdentifier("gotoMap", sender: self)
         //}
     }
 
